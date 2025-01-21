9f881c60da5d56c4127ae13381f563775a9ba797
Veilen1 <devrientv@gmail.com>
2025-01-21 15:37:48 -0300

    Inicializing

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.get('/api', (req, res) => res.send('API funcionando'));
+
+// Conexión con MongoDB
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+  })
+  .catch((err) => console.error(err));
