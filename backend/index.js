d7ba3abb5124b420d3d875fd54de2edfa4f4274d
Veilen1 <devrientv@gmail.com>
2025-01-21 16:19:44 -0300

    Deploying vercel

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,16 @@ app.use(express.json());
 
 // Rutas
 app.get('/api', (req, res) => res.send('API funcionando'));
+app.get('/', (req, res) => res.send('API funcionando'));
 
 // Conexión con MongoDB
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
   })
   .catch((err) => console.error(err));
+
+
+  module.exports = app;
