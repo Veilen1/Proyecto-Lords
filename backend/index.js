32d7e8a9800994516a57a6367f9ae533f3bd76b5
Veilen1 <devrientv@gmail.com>
2025-01-24 19:37:39 -0300

    Express funcionando

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -11,11 +12,16 @@ app.use(express.json());
 
 // Rutas
 app.get('/api', (req, res) => res.send('API funcionando'));
-app.get('/', (req, res) => res.send('API funcionando'));
-
+app.get('/', (req, res) => res.send('API raiz funcionando'));
 
 // Conexión con MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch((err) => console.error(err));
+
+// Iniciar la aplicación en el puerto especificado
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
